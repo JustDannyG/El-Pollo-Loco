d189edec06d778c6f1cbfd839dc1523ead0062d7
Danny Gruchmann <dannygruchmann@MacBook-Air-von-Danny.local>
2025-10-06 15:43:03 +0700

    add mini Chicken

--- levels/level1.js
+++ levels/level1.js
@@ -8,6 +8,10 @@ function initLevel() {
             new Chicken(),
             new Chicken(),
             new Chicken(),
+            new MiniChicken(),
+            new MiniChicken(),
+            new MiniChicken(),
+            new MiniChicken(),
             new Endboss()
         ],
         [
---
+++ models/miniChicken.class.js
@@ -0,0 +1,50 @@
+class MiniChicken extends MovableObject {
+    
+    // same as chicken but smaller
+    y = 360;
+    height = 60;
+    width = 60;
+
+    imagesWalking = [
+        './img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        './img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        './img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
+    ];
+
+    imagesDead = [
+        './img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+    ];
+
+    constructor() {
+        super();
+        this.offset = {
+            top: 10,
+            left: 5,
+            right: 5,
+            bottom: 10
+        };
+        this.loadImage(this.imagesWalking[0]);
+        this.loadImages(this.imagesWalking);
+        this.loadImages(this.imagesDead);
+        this.animate();
+        this.x = 500 + Math.random() * 3600;
+        this.speed = 0.4 + Math.random() * 0.3;
+    }
+
+    animate() {
+        setInterval(() => {
+            if (!this.isDead()) {
+                this.moveLeft();
+            }
+        }, 1000 / 60);
+        setInterval(() => {
+            if (this.isDead()) { 
+                this.img = this.imageCache[this.imagesDead[0]];
+                this.y = 380;
+                this.speed = 0;
+            } else {
+                this.playAnimation(this.imagesWalking);
+            }
+        }, 1000 / 10);
+    }; 
+}
